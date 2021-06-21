990468df17649d14e6142c777aa8f2d1e1ac30a1
yangbq <yangbq@qq.com>
2021-06-21 15:08:28 +0800

    fix homepage

--- scripts/deploy_gitee.sh
+++ scripts/deploy_gitee.sh
@@ -5,6 +5,6 @@ cd build &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
-git remote add origin git@gitee.com:frankfang/morney-13-website.git &&
+git remote add origin git@gitee.com:qqqq12337/morney-website.git &&
 git push -u origin master -f
 cd -
